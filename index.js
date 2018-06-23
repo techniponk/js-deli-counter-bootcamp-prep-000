1ec734fee6948799eeafb7e26a8f71106aa4b45e
Nikki M <nrmonegain@gmail.com>
2018-06-23 02:44:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,11 +19,11 @@ function nowServing (katzDeliLine) {
 
 function currentLine (katzDeliLine) {
   
-  if (katzDeliLine.length ===0) {
+  if (katzDeliLine.length === 0) {
     return "The line is currently empty."
   } else {
     for (var i = 0; i < katzDeliLine.length - 1; i++){
-      
+      return "The line is currently: " + (i + 1) + ". " + katzDeliLine[i]
     }
   }
 }

3bc71c3508fc35121458cc75504a8dba961da23e
Nikki M <nrmonegain@gmail.com>
2018-06-23 02:41:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,5 +19,11 @@ function nowServing (katzDeliLine) {
 
 function currentLine (katzDeliLine) {
   
-  
+  if (katzDeliLine.length ===0) {
+    return "The line is currently empty."
+  } else {
+    for (var i = 0; i < katzDeliLine.length - 1; i++){
+      
+    }
+  }
 }

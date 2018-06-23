ce4cfa15af278b1d5013a678750518ac3fad7bc3
Nikki M <nrmonegain@gmail.com>
2018-06-23 02:09:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function nowServing (katzDeliLine) {
   if (katzDeliLine.length === 0) {
   return ("There is nobody waiting to be served!")
   } else {
-    return "Currently serving " + katzDeliLine[0] + "."
+    //return "Currently serving " + katzDeliLine[0] + "."
     katzDeliLine.shift()
   }
 }

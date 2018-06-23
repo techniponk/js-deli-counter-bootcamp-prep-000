f71c7ade58b21eda935a880e6f71b63df0bc2e57
Nikki M <nrmonegain@gmail.com>
2018-06-23 02:20:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function nowServing (katzDeliLine) {
   return ("There is nobody waiting to be served!")
   } else {
     return "Currently serving " + katzDeliLine[0] + "."
-    katzDeliLine.shift()
+   return katzDeliLine.shift()
   }
 }
 

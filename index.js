66db9bc43f00ff2b376139c5ad6d3ec486a30aa4
Nikki M <nrmonegain@gmail.com>
2018-06-23 02:27:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ function takeANumber (katzDeliLine, name) {
 }
 
 function nowServing (katzDeliLine) {
+  var firstPerson = katzDeliLine[0]
   
   if (katzDeliLine.length === 0) {
   return ("There is nobody waiting to be served!")
